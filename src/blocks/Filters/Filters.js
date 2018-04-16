8622dfcd2bfb927021c3bcafc1c9d5313d056ba0
ermojob <ermojob@gmail.com>
2018-04-16 21:53:15 +0300

    master

---
+++ src/blocks/Filters/Filters.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Slider from 'material-ui/Slider';
+
+class Filters extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <Slider />
+      </div>
+    );
+  }
+}
+
+export default Filters;
