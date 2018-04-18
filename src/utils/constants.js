1377310e5ab769e8bdc6d559bd1e32561dc13777
ermojob <ermojob@gmail.com>
2018-04-18 21:34:58 +0300

    Added img url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
 export const TOKEN = 'api_key=6591c5dd27b8837aebb906cbf9754b66';
 export const POPULAR_FILMS =
   'https://api.themoviedb.org/3/movie/popular?api_key=6591c5dd27b8837aebb906cbf9754b66&language=en-US&page=';
+export const IMAGE_URL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2/';
