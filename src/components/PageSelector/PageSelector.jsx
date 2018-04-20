a3c3c47e08c44b3867c574a942239d935c4904d6
ermojob <ermojob@gmail.com>
2018-04-20 12:55:12 +0300

    made pagination working

--- src/components/PageSelector/PageSelector.jsx
+++ src/components/PageSelector/PageSelector.jsx
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 
-const PageSelector = props => {
-  const { number, getFilmsCollection } = props;
-  return <div className="PageSelector">{number}</div>;
-};
+class PageSelector extends Component {
+  showList() {
+    const { number, getFilmsCollection } = this.props;
+    console.log(number);
+    getFilmsCollection(number);
+  }
+  render() {
+    const { number, getFilmsCollection } = this.props;
+    return (
+      <div className="PageSelector" onClick={this.showList.bind(this)}>
+        {number}
+      </div>
+    );
+  }
+}
 
 export default PageSelector;
