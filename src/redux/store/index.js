87b01a5c8670e8e665fdb56819ad1dc105e868f8
ermojob <ermojob@gmail.com>
2018-04-22 22:28:36 +0300

    Added redux dev-tools to project

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -2,13 +2,15 @@ import { createStore, applyMiddleware } from 'redux';
 import reducer from '../reducers';
 import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 export default function configureStore(initialState) {
   const logger = createLogger();
   const store = createStore(
     reducer,
     initialState,
-    applyMiddleware(thunk, logger),
+
+    composeWithDevTools(applyMiddleware(thunk, logger)),
   );
   return store;
 }
