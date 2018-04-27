caee677754a1c6e9da50046e3868227d0e02c331
ermojob <ermojob@gmail.com>
2018-04-28 00:26:25 +0300

    switched components to page

---
+++ src/containers/MoviePage/MoviePage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class MoviePage extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <div>Film page</div>;
+  }
+}
+
+export default MoviePage;
