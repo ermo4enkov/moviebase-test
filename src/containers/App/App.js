1e39bcd095fa1c1a5655194f24789581d934fc19
ermojob <ermojob@gmail.com>
2018-04-28 21:23:26 +0300

    Cleaned up before add routing

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,14 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
-import logo from '../../assets/logo.svg';
 import './App.css';
-import MovieList from '../MovieList';
-import Pagination from '../Pagination';
-import SearchFilm from '../SearchFilm';
 import MoviesCollectionPage from '../../components/MoviesCollectionPage/MoviesCollectionPage';
-
 import getFilmsCollection from '../../redux/actions/getFilmsCollection';
 
 import { bindActionCreators } from 'redux';
@@ -20,15 +14,7 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <div className="App">
-          <header className="App-header">
-            <h1 className="App-title">Welcome to Films base</h1>
-            {/* <SearchFilm /> */}
-          </header>
-          {/* <MovieList />
-          <Pagination /> */}
-          <MoviesCollectionPage />
-        </div>
+        <MoviesCollectionPage />
       </MuiThemeProvider>
     );
   }
