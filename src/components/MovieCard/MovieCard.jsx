d000cc5c355a44cf56dbe11fc8803292ccbf5e22
ermojob <ermojob@gmail.com>
2018-05-01 21:44:34 +0300

    added button to fav

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -7,14 +7,17 @@ import RaisedButton from 'material-ui/RaisedButton';
 const MovieCard = props => {
   const link = `/movie/:id${props['id']}`;
   return (
-    <Link className="MovieCard" to={link}>
+    <div className="MovieCard" >
       <img src={`${IMAGE_URL}${props['poster_path']}`} alt="poster" to={link}/>
       <div className="MovieCard__about">
         <div className="MovieCard__title">{props['title']}</div>
         <div className="MovieCard__overview">{props['overview']}</div>
+        <Link to={link}>
+            <RaisedButton>More detail...</RaisedButton>
+        </Link>
         <RaisedButton>Add to favorites</RaisedButton>
       </div>
-    </Link>
+    </div>
   );
 };
 
